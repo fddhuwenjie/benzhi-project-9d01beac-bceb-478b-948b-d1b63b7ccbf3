5996e5d89cd49ab70d8900fccfbc697aba3fcbf7
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for 9d01beac-bceb-478b-948b-d1b63b7ccbf3-bug-4

--- internal/audit/engine.go
+++ internal/audit/engine.go
@@ -3,27 +3,36 @@ package audit
 import (
 	"benzhi-project-9d01beac-bceb-478b-948b-d1b63b7ccbf3/internal/domain"
 	"sort"
+	"strconv"
 	"sync"
 )
 
 type Engine struct {
 	rules []Rule
 	mu    sync.RWMutex
-	cache map[int64][]domain.AuditIssue
+	cache map[string][]domain.AuditIssue
 }
 
 func NewEngine() *Engine {
 	return &Engine{
 		rules: []Rule{HeadingRule{}, LinkTextRule{}, ImageAltRule{}, TableHeaderRule{}},
-		cache: make(map[int64][]domain.AuditIssue),
+		cache: make(map[string][]domain.AuditIssue),
 	}
 }
 
 func (e *Engine) Version() string { return RuleVersion }
 
+func (e *Engine) cacheKey(content domain.DocumentContent) string {
+	// Revision is per-case, so the cache must be keyed by both CaseID and
+	// Revision to prevent issues from one acceptance case leaking into
+	// another case that happens to share the same revision number.
+	return content.CaseID + "@" + strconv.FormatInt(content.Revision, 10)
+}
+
 func (e *Engine) Run(content domain.DocumentContent) []domain.AuditIssue {
+	key := e.cacheKey(content)
 	e.mu.RLock()
-	cached, ok := e.cache[content.Revision]
+	cached, ok := e.cache[key]
 	e.mu.RUnlock()
 	if ok {
 		return append([]domain.AuditIssue(nil), cached...)
@@ -44,7 +53,7 @@ func (e *Engine) Run(content domain.DocumentContent) []domain.AuditIssue {
 		issues = append(issues, domain.AuditIssue{ID: domain.StableID("issue", content.CaseID, f.RuleCode, f.BlockID), CaseID: content.CaseID, RuleCode: f.RuleCode, RuleVersion: RuleVersion, Severity: f.Severity, BlockID: f.BlockID, Message: f.Message, DetectedContentRevision: content.Revision, Status: domain.IssueOpen})
 	}
 	e.mu.Lock()
-	e.cache[content.Revision] = append([]domain.AuditIssue(nil), issues...)
+	e.cache[key] = append([]domain.AuditIssue(nil), issues...)
 	e.mu.Unlock()
 	return issues
 }
